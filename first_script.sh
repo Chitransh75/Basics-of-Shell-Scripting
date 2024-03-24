b5de15e142cbcbd80273d7e089b5a28ee5b8cee2
Chitransh75 <chitransh057@gmail.com>
2024-03-24 18:21:11 +0530

    First script

---
+++ first_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+# How to Create Your First Bash Script.
+
+
+#Let's create a simple script in bash that outputs Hello World.
+
+#Create a file named "frist_script.sh"
+
+# using "touch" command.
+
+# "touch first_script.sh" then open it in nano editior or vim as you like.  
+
+#         OR
+
+# "nano first_script.sh" it directly  open in nano editior.
+
+# MAIN SCRIPT :-
+echo "Hello, this is my first script "     
+
+
+# then, provide execution rights to your file using " chmod 775 or 777" it means 'read write execute' .
+
+# Run with this command in terminal "bash first_script.sh" OR "./ first_script.sh"
+    
+
