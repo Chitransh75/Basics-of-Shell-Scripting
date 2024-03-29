fdf571bbee63cc402a759290c371f9ff48629420
Chitransh75 <chitransh057@gmail.com>
2024-03-29 12:52:56 +0530

    variables

--- create_variable.sh
+++ create_variable.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+<<CO
+Scripts start with a bash bang.
+Scripts are also identified with a shebang. Shebang is a combination of bash "#" and bang "!" followed the the bash shell path. This is the first line of the script. Shebang tells the shell to execute it via bash shell.
 
-#Scripts start with a bash bang.
-#Scripts are also identified with a shebang. Shebang is a combination of bash "#" and bang "!" followed the the bash shell path. This is the first line of the script. Shebang tells the shell to execute it via bash shell.
-
-# '$' denote variable 
-
+ '$' denote variable 
+CO
 greeting=Hello
 
 name=Chitransh
