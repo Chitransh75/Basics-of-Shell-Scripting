f4ae193a4c0bda193a83d7f9d72ed8a366fe1ccd
Chitransh75 <chitransh057@gmail.com>
2024-03-30 12:48:16 +0530

    For loop

---
+++ forloop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#for loop is a control flow statement that allows code or commands to be repeatedly executed based on a condition.
+
+for i in {1..10}
+do
+echo "$i"
+done
+
+for x in Ubuntu Windows Mac RHEL are OS
+do
+echo "$x"
+done
+
